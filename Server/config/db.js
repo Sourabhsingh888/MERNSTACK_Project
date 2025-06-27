e9d7b7dc5c10735f4cc0bf993a7f6b4ee8006b6a
sourabh singh bhadoriya <bhadoriyasourabh707@gmail.com>
2025-06-27 08:41:48 +0530

    Fix two files index.js and db.js

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.mongodb_url);
+    await mongoose.connect(process.env.mongodb_url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     console.log("DB connected successfully");
   } catch (error) {
-    console.error(error);
+    console.error("MongoDB connection error:", err.message);
   }
 };
 module.exports = { dbConnect };
